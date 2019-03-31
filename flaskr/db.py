c927810592d561783fabc2d427e59a150667f112
Isak Ernstig <ernstig1@gmail.com>
2019-03-31 12:15:45 +0200

    added init_app to enable database instantiation

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,9 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
 
 def get_db():
     if 'db' not in g:
