c016fc23134fe67dd8c378ae6588508a16829dc9
Isak Ernstig <ernstig1@gmail.com>
2019-08-23 13:52:39 -0400

    added authentification file, register and login

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,6 +4,7 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
@@ -24,6 +25,7 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
 
 
 def init_db():
