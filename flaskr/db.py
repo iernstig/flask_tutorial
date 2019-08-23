590f27dcd1ec53f0be46c9e0eb7d703d662271c3
Isak Ernstig <ernstig1@gmail.com>
2019-08-22 22:04:39 -0400

    added some white space

--- flaskr/db.py
+++ flaskr/db.py
@@ -4,9 +4,11 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
 
 def get_db():
     if 'db' not in g:
